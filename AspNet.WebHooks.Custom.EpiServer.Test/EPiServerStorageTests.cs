3fcefbcfcc2508f9af1a64dea6452e94e6e12ed1
khurram khan <khurramkhang@outlook.com>
2016-01-04 02:35:49 +0000

    Test

---
+++ AspNet.WebHooks.Custom.EpiServer.Test/EPiServerStorageTests.cs
@@ -0,0 +1,29 @@
+﻿namespace AspNet.WebHooks.Custom.EpiServer.Test
+{
+    using AspNet.WebHooks.Custom.EpiServerStorage.Extensions;
+    using AspNet.WebHooks.Custom.EpiServerStorage.WebHooks;
+    using Microsoft.AspNet.WebHooks;
+    using Microsoft.AspNet.WebHooks.Services;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    [TestFixture]
+    public class EPiServerStorage_Tests
+    {
+        [Test]
+        public void It_Is_EpiWebHookStore()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.InitializeCustomWebHooksEPiServerStorage();
+            IWebHookStore actual = CustomServices.GetStore();
+
+            Assert.IsInstanceOf<EpiWebHookStore>(actual);
+        }
+    }
+}
